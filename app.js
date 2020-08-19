366dfda92c59a6e39563b2d13c219bd3d363b4b4
dotpxg <dotpxg@gmail.com>
2020-08-20 04:01:33 +0530

    First Commit: Created a Basic Express Web Server using node js

---
+++ app.js
@@ -0,0 +1,25 @@
+// const express = require('express');
+
+// //express app
+// const app = express();
+
+// //listen for requests
+// app.listen(3000, () => {
+//     console.log('App listening at Port 3000')
+// });
+
+// app.get('/', (req, res) => {
+
+//     res.send('<p> This is a test response!</p>');
+// })
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('This is a Test Response!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
