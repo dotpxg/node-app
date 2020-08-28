23e195b189c23510dad3c5896d05906149302e30
Pratik Xavier <dotpxg@gmail.com>
2020-08-29 02:12:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,14 @@
 // })
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('This is a Test Response!')
 })
 
-app.listen(port,'0.0.0.0',() => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = app;
