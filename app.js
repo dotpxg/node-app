b476b6892ab51ce3e4c8b788e80ef993caf890bb
Pratik Xavier <dotpxg@gmail.com>
2020-08-29 20:22:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// const express = require('express');
+ const express = require('express');
 
-// //express app
-// const app = express();
+ //express app
+ const app = express();
+ //listen for requests
+ app.listen(3000, () => {
+     console.log('App listening at Port 3000')
+ });
 
-// //listen for requests
-// app.listen(3000, () => {
-//     console.log('App listening at Port 3000')
-// });
+ app.get('/', (req, res) => {
 
-// app.get('/', (req, res) => {
+     res.send('<p> This is a test response!</p>');
+ })
 
-//     res.send('<p> This is a test response!</p>');
-// })
-const express = require('express')
-const app = express()
+//const express = require('express')
+//const app = express()
 //const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+//app.get('/', (req, res) => {
   res.send('This is a Test Response!')
 })
 
-app.listen(3000,() => {
-  console.log(`Example app listening at http://localhost:${port}`)
+//app.listen(3000,() => {
+  //console.log(`Example app listening at http://localhost:${port}`)
 })
 
-module.exports = app;
+//module.exports = app;
